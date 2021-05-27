67e9aa8af189d3288deaaf9f3feb194f508f36ff
gali nickel <gali.nickel95@gmail.com>
2021-05-28 01:13:41 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class App extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className="App ui container">
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
+
